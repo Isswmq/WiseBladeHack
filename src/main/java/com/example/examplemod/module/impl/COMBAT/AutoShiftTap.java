1b3bc79eece3becc4c772565fcfaa96b844df84b
ilya <isswmq@gmail.com>
2023-08-03 13:46:47 +0300

    Recode AutoShiftTap

--- src/main/java/com/example/examplemod/module/impl/COMBAT/AutoShiftTap.java
+++ src/main/java/com/example/examplemod/module/impl/COMBAT/AutoShiftTap.java
@@ -5,6 +5,10 @@ import net.minecraft.network.play.client.CEntityActionPacket;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class AutoShiftTap extends Module {
 
     public AutoShiftTap(){
@@ -13,8 +17,22 @@ public class AutoShiftTap extends Module {
 
     @SubscribeEvent
     public void onAttack(AttackEntityEvent event){
-        mc.player.input.shiftKeyDown = true;
-        mc.getConnection().send(new CEntityActionPacket(mc.player, CEntityActionPacket.Action.RELEASE_SHIFT_KEY));
-        mc.getConnection().send(new CEntityActionPacket(mc.player, CEntityActionPacket.Action.PRESS_SHIFT_KEY));
+        if(mc.player != null){
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mc.player.input.shiftKeyDown = true;
+                    Objects.requireNonNull(mc.getConnection()).send(new CEntityActionPacket(mc.player, CEntityActionPacket.Action.PRESS_SHIFT_KEY));
+                }
+            }, 20);
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Objects.requireNonNull(mc.getConnection()).send(new CEntityActionPacket(mc.player, CEntityActionPacket.Action.RELEASE_SHIFT_KEY));
+                }
+            },50);
+        }
     }
 }
